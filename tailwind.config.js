38bc022e99621f087126ad701fbfdf6e832880c4
Polina Kerkenyakova <polina.kerkenyakova@gmail.com>
2022-04-10 13:54:16 +0300

    Add custom colors and media queries

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,22 @@
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ['./src/**/*.{html,js}'],
   theme: {
-    extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
+    extend: {
+      colors: {
+        'bright-red': 'hsl(12, 88%, 59%)',
+        'dark-blue': 'hsl(228, 39%, 23%)',
+        'dark-graish-blue': 'hsl(227, 12%, 61%)',
+        'very-dark-blue': 'hsl(233, 12%, 13%)',
+        'very-pale-red': 'hsl(13, 100%, 96%)',
+        'very-light-gray': 'hsl(0, 0%, 98%)'
+      }
+    },
   },
   plugins: [],
 }
